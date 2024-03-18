d2ceda32ce390f77cd6f3251d53ff9669bf09502
“emansarahafi” <emansarahafi@gmail.com>
2024-03-18 21:46:18 +0100

    Updates to make sure dtabase works correctly.

--- my-app/src/database/superbase.ts
+++ my-app/src/database/superbase.ts
@@ -1,12 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Fetch the API key from an environment variable
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
-const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
-// Check if the environment variables are set
 if (!SUPABASE_URL || !SUPABASE_KEY) {
-    throw new Error("Supabase URL or key is missing. Make sure to set REACT_APP_SUPABASE_URL and REACT_APP_SUPABASE_KEY environment variables.");
+    throw new Error("Supabase URL or key is missing. Make sure to set NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_KEY environment variables.");
 }
 
 export default createClient(SUPABASE_URL, SUPABASE_KEY);
