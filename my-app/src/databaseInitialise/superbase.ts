788b7d687450baaaf37f389de58def40d2db53e7
“emansarahafi” <emansarahafi@gmail.com>
2025-04-03 15:01:10 +0100

    Applied .gitignore to the repository.

--- my-app/src/databaseInitialise/superbase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export default  createClient(
-    "https://vkshrxshqqldxzrbfkck.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrc2hyeHNocXFsZHh6cmJma2NrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk4NzEwOTIsImV4cCI6MjAxNTQ0NzA5Mn0.HeCYO4nUQt88f9MtYGBEHqlz0KLVxCRxqB3xhzVjVPw"
-);
